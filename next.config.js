5bc849b0d8bf62e0975e253257c3d2f954724495
fleurinedave8-rgb <fleurinedave8@gmail.com>
2026-03-18 12:59:20 -0400

    force rebuild netlify

--- next.config.js
+++ next.config.js
@@ -1,4 +1,4 @@
-/** @type {import('next').NextConfig} */
+/** @type {import('next').NextConfig} */ // build 1773852745
 const nextConfig = {
   reactStrictMode: false,
   eslint: { ignoreDuringBuilds: true },
