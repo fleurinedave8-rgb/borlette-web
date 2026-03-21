af51e03f9442cf8da660eeac6a56447283641902
fleurinedave8-rgb <fleurinedave8@gmail.com>
2026-03-20 21:13:44 -0400

    francais tchala ssl

--- next.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: false,
-  eslint: { ignoreDuringBuilds: true },
-  typescript: { ignoreBuildErrors: true },
-  async rewrites() { return []; },
-}
-module.exports = nextConfig;
